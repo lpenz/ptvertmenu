97e10ceb47567bc3ee305a4988397e1e1e61e321
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-10-28 21:17:12 +0100

    Basic vertical menu module, with ptvertmenu-man for tests

--- src/ptvertmenu/__init__.py
+++ src/ptvertmenu/__init__.py
@@ -2,6 +2,14 @@
 
 import importlib.metadata
 
+from .vertmenu import VertMenu
+
 
 def version() -> str:
     return importlib.metadata.version("ptvertmenu")
+
+
+__all__ = [
+    "version",
+    "VertMenu",
+]
