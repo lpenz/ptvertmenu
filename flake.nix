a811de2fdca2d2564edef0c1c921049ea667eb4e
Leandro Lisboa Penz <lpenz@lpenz.org>
2024-01-07 17:25:06 +0000

    Increment version to 0.2.2

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         python = pkgs.python3Packages;
         ptvertmenu = pkgs.python3Packages.buildPythonPackage {
           pname = "ptvertmenu";
-          version = "0.2.1";
+          version = "0.2.2";
           src = self;
           requirements = builtins.readFile ./requirements.txt;
           propagatedBuildInputs = [
